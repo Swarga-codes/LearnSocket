74332306c892092023e6e2e37bc4193a3a3bb304
Swarga-codes <sd3526@srmist.edu.in>
2023-04-17 21:15:10 +0530

    removed cors error and added ui

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const app=require('express')();
 const server=require('http').createServer(app);
-const io=require('socket.io')(server);
+const io=require('socket.io')(server,{
+    cors: {
+      origin: "*",
+      allowedHeaders: ["GET","POST"],
+      credentials: true
+    }
+  });
 io.on('connection',(socket)=>{
     console.log('Connected to socket...',socket);
     socket.on('chat',(payload)=>{
--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect,useState } from 'react';
+import { io } from 'socket.io-client';
 function App() {
+  const[chat,setChat]=useState([]);
+  const[message,setMessage]=useState("");
+const socket=io.connect('http://localhost:5000/');
+  const sendChat=(e)=>{
+e.preventDefault();
+socket.emit('chat',{message});
+setMessage('');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form onSubmit={sendChat}>
+       <h1>My chat application</h1>
+       <input type="text" name='text' value={message} onChange={(e)=>setMessage(e.target.value)}/>
+       <button type='submit'>Send</button>
+       </form>
       </header>
     </div>
   );
