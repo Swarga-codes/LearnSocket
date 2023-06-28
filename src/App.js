9c6d58167be646170311b78251b32938365568b9
Swarga-codes <sd3526@srmist.edu.in>
2023-06-28 16:35:27 +0530

    fixed multiple callbacks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import './App.css';
 import { useEffect,useState } from 'react';
 import { io } from 'socket.io-client';
+const socket=io.connect('http://localhost:5000/');
 function App() {
   const[chat,setChat]=useState([]);
   const[message,setMessage]=useState("");
   const[userName,setUserName]=useState("");
-const socket=io.connect('http://localhost:5000/');
 const fetchChats=async()=>{
   const response=await fetch('http://localhost:5000/chats');
   const data=await response.json()
   console.log(data)
+  setChat(data)
 }
   const sendChat=(e)=>{
 e.preventDefault();
 if(message){
 socket.emit('chat message',{message,userName});
+
+
 setMessage('');
+
 }
   }
   useEffect(()=>{
-    fetchChats()
-    socket.on('chat message',(payload)=>{
-      setChat([...chat,payload]);
-    })
+  
+socket.on('chat message',(payload)=>{
+  console.log(payload)
+fetchChats()
+
+})
     
   },[chat])
+  useEffect(()=>{
+    return()=>{
+    fetchChats()
+    }
+  },[])
 useEffect(()=>{
+
   return()=>{
+    
 setUserName(prompt('Enter your name?'))
-console.log(userName)
 
   }
 },[])
@@ -36,14 +48,19 @@ console.log(userName)
     <div className="App">
       <header className="App-header">
       <h1>My chat application</h1>
+      <div className="chat_display">
       {
         chat.map((payload,idx)=> 
         (
-         <p key={idx}>{payload.userName}:{payload.message}</p>
+         <p key={idx}>{payload.senderName}:{payload.chatContent}</p>
         ))
       }
-      <form onSubmit={sendChat}>
-       <input type="text" name='text' value={message} onChange={(e)=>setMessage(e.target.value)}/>
+      </div>
+      <form onSubmit={
+        sendChat
+      }>
+       <input type="text" name='text' value={message} onChange={(e)=>{setMessage(e.target.value)
+      }}/>
        <button type='submit'>Send</button>
        </form>
       </header>
