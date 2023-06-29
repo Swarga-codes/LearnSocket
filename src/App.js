642b923fa839d2f3ae9b0a7355e8c4e9f4a454a4
Swarga-codes <sd3526@srmist.edu.in>
2023-06-29 20:55:37 +0530

    built chat sec ui

--- src/App.js
+++ src/App.js
@@ -17,18 +17,13 @@ setMessage('');
 }
   }
   useEffect(()=>{
-  
-socket.on('chat message',(payload)=>{
-  console.log(payload)
 
-})
 socket.emit('join')
 socket.on('join',(data)=>{
-
-  console.log('socket data:',data)
   setChat(data)
 })
-  },[chat])
+
+  },[sendChat])
 useEffect(()=>{
 
   return()=>{
@@ -37,17 +32,31 @@ setUserName(prompt('Enter your name?'))
 
   }
 },[])
+localStorage.setItem('name',userName)
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>My chat application</h1>
+     <div className="chatapp">
+    <div className="heading">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
+  </svg>
+<h1>Chat App</h1>
+   
+    </div>
       <div className="chat_display">
       {
         chat?.map((payload,idx)=> 
         (
-         <p key={idx}><b>{payload.senderName}</b>:{payload.chatContent}</p>
+          <div className='chat'>
+         <p key={idx}><b>{payload.senderName}</b></p>
+         <div className="chat_content">
+         {payload.chatContent}
+         </div>
+         </div>
+
         ))
       }
+      </div>
       </div>
       <form onSubmit={
         sendChat
@@ -56,7 +65,7 @@ setUserName(prompt('Enter your name?'))
       }}/>
        <button type='submit'>Send</button>
        </form>
-      </header>
+      
     </div>
   );
 }
